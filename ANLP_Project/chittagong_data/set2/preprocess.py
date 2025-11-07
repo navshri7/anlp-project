eed73a158fa1849ad329259a7093972cf202c215
Vamsha1401 <97653997+Vamsha1401@users.noreply.github.com>
2025-11-07 16:51:47 +0530

    Delete ANLP_Project/chittagong_data directory

--- ANLP_Project/chittagong_data/set2/preprocess.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/set2/dataset.csv')
-last_col = df.columns[-1]
-df[last_col] = df[last_col].astype(str).apply(lambda x: x.split(',')[0] if ',' in x else x)
-df = df[df[last_col] != 'nan']
-df.to_json('data/set2/processed_data.json', orient='records', indent=2, force_ascii=False)
-print("Data saved to JSON file. Number of records:", len(df))
